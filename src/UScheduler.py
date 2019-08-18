1858260f0acecadd234fa194954e69bdb6c9ec99
Igor Neaga <neagaigori@gmail.com>
2019-08-17 21:25:24 -0500

    Changes file date

--- src/UScheduler.py
+++ src/UScheduler.py
@@ -1,14 +1,20 @@
 from src import interface
 from tkinter import *
 import os
+import time
+import datetime
 
 
 def create_interface(argv):
+    cwd = os.getcwd()
+    # Change file date for Auto-Updates purpose
+    current_date_time = datetime.datetime.now()
+    modified_time = time.mktime(current_date_time.timetuple())
+    os.utime(cwd + '\\src\\UScheduler.exe', (modified_time, modified_time))
 
     root = Tk()
     root.title('Uni-Scheduler')
     root.geometry("659x337")
-    cwd = os.getcwd()
     root.iconbitmap(cwd + '\\src\\assets\\unischeduler_icon.ico')
     interface.UserInterface(root)
     root.mainloop()
