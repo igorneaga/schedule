ef3a680ffa4518be4ffd005a36e4379f459e238c
Igor Neaga <neagaigori@gmail.com>
2019-11-10 12:19:49 -0600

    Creates an empty excel file for each department

--- src/payroll_table.py
+++ src/payroll_table.py
@@ -6,12 +6,14 @@ import datetime
 import os
 import csv
 import openpyxl
+import string
+
 from pprint import pprint
 
 
 class PayrollTable:
-    def __init__(self):
-        self.courses_dict = [{'Course': 'BUS 295-1', 'Room': 'AH 0101', 'Course_Days': ['Tuesday'], 'Row': 4, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Credits': 2, 'Course_Title': 'Prep for Bus Careers', 'Type': ['Classroom'], 'Enrollment': 6, 'Faculty': 'Diegnau, Melissa                             ', 'Semester': 'Fall', 'Start_Time': '09:00', 'End_Time': '10:50', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}, {'Course': 'BUS 295-2', 'Room': 'MH 0102', 'Course_Days': ['Tuesday'], 'Row': 5, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Credits': 2, 'Course_Title': 'Prep for Bus Careers', 'Type': ['Classroom'], 'Enrollment': 8, 'Faculty': 'Diegnau, Melissa                             ', 'Semester': 'Fall', 'Start_Time': '11:00', 'End_Time': '12:50', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}, {'Course': 'BUS 295-3', 'Credits': 2, 'Course_Title': 'Prep for Bus Careers', 'Faculty': 'Diegnau, Melissa                             ', 'Enrollment': 17, 'Course_Days': [], 'Start_Time': 'Online', 'End_Time': 'Online', 'Type': ['Online'], 'Row': 6, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}, {'Course': 'BUS 397-1', 'Room': 'MH 0102', 'Course_Days': ['Monday', 'Wednesday'], 'Row': 7, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Credits': 3, 'Course_Title': 'IBE Practicum', 'Type': ['Classroom'], 'Enrollment': 2, 'Faculty': 'Bowyer, Shane                                ', 'Semester': 'Fall', 'Start_Time': '02:10', 'End_Time': '03:20', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}, {'Course': 'BUS 397-2', 'Room': 'PH 0114', 'Course_Days': ['Monday'], 'Row': 8, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Credits': 3, 'Course_Title': 'IBE Practicum', 'Type': ['Classroom'], 'Enrollment': 1, 'Faculty': 'Scott, Kristin                               ', 'Semester': 'Fall', 'Start_Time': '02:10', 'End_Time': '03:20', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}, {'Course': 'BUS 397-3', 'Room': 'HC 1700 B', 'Course_Days': ['Monday'], 'Row': 9, 'File': 'C:/Users/Igor/Desktop/fixing/ACCT_2020.xlsx', 'Sheet_Name': 'Accounting_2019', 'Credits': 3, 'Course_Title': 'IBE Practicum', 'Type': ['Classroom'], 'Enrollment': 1, 'Faculty': 'Severns, Roger                               ', 'Semester': 'Fall', 'Start_Time': '02:10', 'End_Time': '03:20', 'Start_Date': datetime.datetime(2020, 1, 13, 0, 0), 'End_Date': datetime.datetime(2020, 5, 8, 0, 0), 'Department': 'Business'}]
+    def __init__(self, courses_dict):
+        self.courses_dict = courses_dict
         self.allowed_departments = [
             'Accounting', 'Business Law', 'Business', 'Finance',
             'Marketing & International Business', 'MBA', 'MACC',
@@ -21,10 +23,9 @@ class PayrollTable:
         self.professor_data = []
         self.faculty_dict = dict()
         self.payroll_dict_format()
+        self.create_excel_table()
 
     def payroll_dict_format(self):
-        allowed_departments = self.allowed_departments
-
         def create_faculty_dict(courses_dict):
             result = dict()
             for course_len in range(len(courses_dict)):
@@ -37,6 +38,11 @@ class PayrollTable:
 
         def get_professor_type(f_dict):
             prof_data = []
+            allowed_departments = [
+                'Accounting', 'Business Law', 'Business', 'Finance',
+                'International Business', 'MBA', 'MACC',
+                'Management', 'Marketing'
+            ]
 
             def fetch_page():
                 url = f'http://www.mnsu.edu/find/people.php?givenname={first_name}&sn={last_name}&employeetype='
@@ -91,36 +97,105 @@ class PayrollTable:
             return cost_center
 
         csv_file_data = get_csv_file('department_cost.csv')
+        final_dict = dict()
         for professor, courses in faculty_dict.items():
             first_name = HumanName(professor).first
             last_name = HumanName(professor).last
+            current_professor = next(
+                (i for i in self.professor_data
+                 if re.search(fr'{first_name}.*?{last_name}', i['name'])),
+                dict()
+            )
+            final_dict[professor] = dict()
+            final_dict[professor]['professor'] = current_professor
+            tmp_courses = []
+
             for course in courses:
-                current_professor = next(
-                    (i for i in self.professor_data
-                     if re.search(fr'{first_name}.*?{last_name}', i['name'])),
-                    dict()
-                )
-                if course['Department'] == 'Business':
-                    if (csv_file_data.get('BUS')).lower() == 'professor':
-                        cost = csv_file_data.get(current_professor.get('department'))
-                        print(cost)
-                        if current_professor.get('department'):
-                            course['Department'] = current_professor['department']
+                if course['Department'].lower() == 'business':
+                    cost = csv_file_data.get(current_professor.get('department'))
+                    if current_professor.get('department'):
+                        course['Department'] = current_professor['department']
                 else:
                     cost = csv_file_data.get(course['Department'])
                 course['Cost'] = cost
+                tmp_courses.append(course)
+            final_dict[professor]['courses'] = tmp_courses
 
-        self.faculty_dict = faculty_dict
+        self.faculty_dict = final_dict
 
     def print_department_info(self, department):
-        filtered_dict = dict()
-        for professor, courses in self.faculty_dict.items():
-            filtered_courses = [course for course in courses if
+        filtered_dict_courses = dict()
+
+        for professor, data in self.faculty_dict.items():
+            filtered_courses = [course for course in data['courses'] if
                                 re.search(fr'.*?{department}.*?', course['Department'])]
             if filtered_courses:
-                filtered_dict[professor] = filtered_courses
-        #pprint(filtered_dict)
-        return filtered_dict
+                filtered_dict_courses[professor] = dict()
+                filtered_dict_courses[professor]['courses'] = filtered_courses
+                filtered_dict_courses[professor]['professor'] = data.get("professor")
+
+        return filtered_dict_courses
 
 
-PayrollTable()
+    def create_excel_table(self):
+        for dep in self.allowed_departments:
+            print(dep)
+            department_courses = self.print_department_info(dep)
+            print(department_courses)
+            if department_courses:
+                self.main_program_controller(dep)
+
+
+    def main_program_controller(self, department):
+        self.create_excel_file()
+
+        self.create_excel_sheet("Payroll Table", True)
+        self.dates_heading()
+        self.instructions_text()
+        self.department_text(department)
+        self.merge_excel_cells()
+        #self.insert_courses()
+
+        self.workbook.save('__excel_files\\' + "Payroll_Test_" + department + ".xlsx")
+
+    def create_excel_file(self):
+        def create_directory():
+            """Creates directory for created excel file"""
+            if not os.path.exists('__excel_files'):
+                os.makedirs('__excel_files')
+        create_directory()
+
+        self.workbook = openpyxl.Workbook()
+
+    def create_excel_sheet(self, sheet_name, first_sheet=False):
+        if first_sheet is True:
+            self.sheet = self.workbook.get_sheet_by_name("Sheet")
+            self.sheet.title = "Payroll Table"
+        else:
+            self.workbook.create_sheet(sheet_name)
+            self.sheet = self.workbook.get_sheet_by_name(sheet_name)
+
+    def dates_heading(self):
+        self.sheet["A1"] = "Table created:"
+        self.sheet["A2"] = datetime.datetime.today().strftime('%m/%d/%Y')
+
+    def instructions_text(self):
+        self.sheet["F1"] = "* Key Concept for Scheduling - *Max Credits for Fall=14"
+        self.sheet["F2"] = "* Final Total Credits must be 24"
+        self.sheet["F3"] = "* Max Credit for the Year 29 with Overload"
+
+    def department_text(self, department):
+        self.sheet["A4"] = department
+
+    def merge_excel_cells(self):
+        # Dates merge cells
+        self.sheet.merge_cells("A1:B1")
+        self.sheet.merge_cells("A2:B2")
+
+        # Instructions merge cells
+        self.sheet.merge_cells("F1:J1")
+        self.sheet.merge_cells("F2:J2")
+        self.sheet.merge_cells("F3:J3")
+
+
+
